4fed79ac8b8195aac87ca20f995dca6b45442e1e
itirbit <twmw@mail.ru>
2021-03-22 09:21:07 +0000

    remove const

--- tour/basics/constants/constants.go
+++ tour/basics/constants/constants.go
@@ -5,13 +5,13 @@ import "fmt"
 const Pi = 3.14
 
 func main() {
-	const World = "Dafuq"
+	World := "Dafuq"
 	World = "change world"
 	fmt.Println("hello", World)
-	Pi = 3.14159
+	Pi := 3.14159
 	fmt.Println("happy", Pi, "Day")
 
-	const Truth = true
+	Truth := true
 	Truth = false
 	fmt.Println("Go rules?", Truth)
 }
