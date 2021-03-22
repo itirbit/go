1a4e732ba5131d5d751d3262398831e0393e2787
itirbit <twmw@mail.ru>
2021-03-22 09:19:02 +0000

    try change const

--- tour/basics/constants/constants.go
+++ tour/basics/constants/constants.go
@@ -6,9 +6,12 @@ const Pi = 3.14
 
 func main() {
 	const World = "Dafuq"
+	World = "change world"
 	fmt.Println("hello", World)
+	Pi = 3.14159
 	fmt.Println("happy", Pi, "Day")
 
 	const Truth = true
+	Truth = false
 	fmt.Println("Go rules?", Truth)
 }
