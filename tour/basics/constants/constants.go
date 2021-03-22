d3a372a2ffab8d10c5f60efd1453c3c05b2109cd
itirbit <twmw@mail.ru>
2021-03-22 09:17:23 +0000

    constants

---
+++ tour/basics/constants/constants.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+func main() {
+	const World = "Dafuq"
+	fmt.Println("hello", World)
+	fmt.Println("happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
