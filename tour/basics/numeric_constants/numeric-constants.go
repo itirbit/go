bc2d735b45fef87dee9ddb4f1f95f9194ac98b9c
itirbit <twmw@mail.ru>
2021-03-23 09:13:43 +0000

    int overflow

--- tour/basics/numeric_constants/numeric-constants.go
+++ tour/basics/numeric_constants/numeric-constants.go
@@ -12,6 +12,7 @@ func needFloat(x float64) float64 { return x * 0.1 }
 
 func main() {
 	fmt.Println(needInt(Small))
+	fmt.Println(needInt(Big))
 	fmt.Println(needFloat(Small))
 	fmt.Println(needFloat(Big))
 }
