01d579d2fe9031b7c16d105d68e4ca5aff6b4658
itirbit <twmw@mail.ru>
2021-04-01 10:09:27 +0000

    stacking defer

--- tour/flow_control/defer/defer.go
+++ tour/flow_control/defer/defer.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
 	defer fmt.Println("world")
 	fmt.Println("hello")
+
+	fmt.Println("counting")
+	for i:=0; i<10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
 }
