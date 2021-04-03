ef32e79e43194652374a12011a9f31764e8a5ad5
itirbit <twmw@mail.ru>
2021-04-03 07:17:58 +0000

    go struct can handle small name variables

--- tour/more_types/structs/structs.go
+++ tour/more_types/structs/structs.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 type Vertex struct {
-	X int
-	Y int
+	x int
+	y int
 }
 
 func main() {
 	fmt.Println(Vertex{1,2})
 	v := Vertex{1,2}
-	v.X = 4
-	fmt.Println(v.X)
+	v.x = 4
+	fmt.Println(v.y)
 }
