5f5f8b6e71a1d926085e5e6605902927ec7a52e8
itirbit <twmw@mail.ru>
2021-04-07 09:31:04 +0000

    slices pointers

--- tour/more_types/arrays/array.go
+++ tour/more_types/arrays/array.go
@@ -19,6 +19,24 @@ func slices(){
 	fmt.Println(s)
 }
 
+func slices_pointers(){
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	a:=names[0:2]
+	b:=names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a,b)
+	fmt.Println(names)
+}
+
 func main(){
-	slices()
+	slices_pointers()
 }
