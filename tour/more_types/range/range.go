20acf7f2ba1121ccbec02f2328346d27899f9864
itirbit <twmw@mail.ru>
2021-04-16 09:39:06 +0000

    range continued

--- tour/more_types/range/range.go
+++ tour/more_types/range/range.go
@@ -2,10 +2,24 @@ package main
 
 import "fmt"
 
-var pow = []int{1,2,4,8,16,32,64,128}
-
-func main(){
+func myrange(){
+	var pow = []int{1,2,4,8,16,32,64,128}
 	for i,v := range pow {
 		fmt.Printf("2**%d = %d\n", i, v)
 	}
+
+}
+
+func range_continued(){
+	pow := make([]int, 10)
+	for i := range pow{
+		pow[i] = 1 << uint(i) // == 2**i
+	}
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
+}
+
+func main(){
+	range_continued()
 }
