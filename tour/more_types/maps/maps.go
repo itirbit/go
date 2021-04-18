9db9554f2dc9b340bf8d5812a46d8ec0483a3940
itirbit <twmw@mail.ru>
2021-04-18 05:06:20 +0000

    go struct in func

--- tour/more_types/maps/maps.go
+++ tour/more_types/maps/maps.go
@@ -2,15 +2,19 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	Lat, Long float64
-}
+func maps(){
 
-var m = map[string]Vertex{
-	"Bell Labs": {40.68433, -74.39967},
-	"Google": {37.42202, -122.08408},
+	type Vertex struct {
+		Lat, Long float64
+	}
+
+	var m = map[string]Vertex{
+		"Bell Labs": {40.68433, -74.39967},
+		"Google": {37.42202, -122.08408},
+	}
+	fmt.Println(m)
 }
 
 func main(){
-	fmt.Println(m)
+	maps();
 }
