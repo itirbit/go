4f2fc9d66d55c09ae3df1f91bd05cee0c27fd82b
itirbit <twmw@mail.ru>
2021-04-26 11:46:14 +0000

    methods

--- tour/methods/methods/methods.go
+++ tour/methods/methods/methods.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"math"
 )
-
 type Vertex struct {
 	X, Y float64
 }
@@ -13,8 +12,19 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+
+func func1(){
+	v := Vertex{3,4}
+	fmt.Println(v.Abs())
+}
+
 func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func func2(){
+	v := Vertex{3,4}
+	fmt.Println(Abs(v))
 }
 
 type MyFloat float64
@@ -26,7 +36,11 @@ func (f MyFloat) Abs() float64 {
 	return float64(f)
 }
 
+func func3(){
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+}
+
 func main(){
-	v := MyFloat(-math.Sqrt2)
-	fmt.Println(v.Abs())
+	func1()
 }
