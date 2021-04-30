c5a2db29fba71ee7a9a66e164acce28ba474249f
itirbit <twmw@mail.ru>
2021-04-30 09:33:59 +0000

    go pointer receiver

--- tour/methods/indirection/indirection.go
+++ tour/methods/indirection/indirection.go
@@ -47,6 +47,13 @@ func indirection2(){
 	fmt.Println(AbsFunc(*p))
 }
 
+func pointer_receiver(){
+	v := &Vertex{3,4}
+	fmt.Printf("Before scaling: %+v, Abs: %v\n", v, v.Abs())
+	v.Scale(5)
+	fmt.Printf("After scaling: %+v, Abs: %v\n", v, v.Abs())
+}
+
 func main(){
-	indirection2()
+	pointer_receiver()
 }
